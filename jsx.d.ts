347e3f7f3a11224f22023633f1fdd445cd099e91
MaiXuanNghia <nghiamaiquangtri@gmail.com>
2024-03-15 21:09:35 +0700

    feature/signin

---
+++ jsx.d.ts
@@ -0,0 +1,8 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+        div: React.HTMLAttributes<HTMLDivElement>;
+        span: React.HTMLAttributes<HTMLSpanElement>;
+        input: React.InputHTMLAttributes<HTMLInputElement>;
+        // Thêm các phần tử khác nếu cần thiết
+    }
+}
--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,26 +1,21 @@
 "use client";
 import Image from "next/image";
+import React, { useState } from "react";
+import axios from "axios";
 import { useRouter } from "next/navigation";
-import React, { useState } from 'react';
-import axios from 'axios';
+import router from "next/router";
 
-
-
-const Signup = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/auth/register', {
-        name,
+      const response = await axios.post('http://127.0.0.1:8000/api/auth/login', {
         email,
         password,
-        password_confirmation: confirmPassword,
       });
 
       console.log(response.data); // Xử lý dữ liệu phản hồi theo nhu cầu
@@ -30,7 +25,7 @@ const Signup = () => {
     } catch (error) {
       console.error(error); // Xử lý lỗi theo nhu cầu
     }
-  };
+  }
 
   return (
     <div className="flex justify-center mt-8 ">
@@ -38,44 +33,24 @@ const Signup = () => {
         <div className="shadow-lg flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <div className="flex justify-center">
-              <Image src="/login.gif" width={70} height={70} />
+              <Image src="/login.gif" width={70} height={70} alt="" />
             </div>
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Create a new account
+              Sign in to your account
             </h2>
           </div>
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form onSubmit={handleSubmit} className="space-y-6" action="#" method="POST">
-            <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Name
-                </label>
-                <div className="mt-2">
-                  <input  onChange={(e) => setName(e.target.value)}
-                    id="name"
-                    name="name"
-                    type="text"
-                    autoComplete="name"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-
+            <form
+              onSubmit={handleSubmit}
+              className="space-y-6"
+              action="#"
+              method="POST"
+            >
               <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Email address
-                </label>
                 <div className="mt-2">
-                  <input onChange={(e) => setEmail(e.target.value)}
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
                     id="email"
                     name="email"
                     type="email"
@@ -96,7 +71,8 @@ const Signup = () => {
                   </label>
                 </div>
                 <div className="mt-2">
-                  <input onChange={(e) => setPassword(e.target.value)}
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
                     id="password"
                     name="password"
                     type="password"
@@ -108,45 +84,24 @@ const Signup = () => {
               </div>
 
               <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="cpassword"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Confirm password
-                  </label>
-                </div>
-                <div className="mt-2">
-                  <input onChange={(e) => setConfirmPassword(e.target.value)}
-                    id="cpassword"
-                    name="cpassword"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign up
+                  Sign ip
                 </button>
               </div>
             </form>
 
             <p className="mt-10 text-center text-sm text-gray-500">
-              Already registed?{" "}
+              Want to crate account?{" "}
               <span
-                href="#"
                 className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor:pointer"
                 onClick={() => {
-                  router().push("/");
+                  router.push("/");
                 }}
               >
-                Sign in
+                Sign up
               </span>
             </p>
           </div>
@@ -156,4 +111,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Signin;
